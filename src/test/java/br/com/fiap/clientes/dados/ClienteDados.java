08b2ac8b0445c3bb0750968c603f2dcc7cddeb2c
Ricardo Rafael Lara Gomes <ricardolara.ti@gmail.com>
2024-05-05 17:42:12 -0300

    test: adicionar testes unitários de integração no service do cliente.

--- src/test/java/br/com/fiap/clientes/dados/ClienteDados.java
+++ src/test/java/br/com/fiap/clientes/dados/ClienteDados.java
@@ -3,12 +3,18 @@ package br.com.fiap.clientes.dados;
 import br.com.fiap.clientes.api.model.ClienteDto;
 import br.com.fiap.clientes.domain.model.Cliente;
 
+import java.time.LocalDate;
+
 public class ClienteDados {
 
     public Cliente criarCliente1(){
         return Cliente.builder()
                 .id(1L)
                 .nome("Bruno Silveira")
+                .email("bruno.silveira@gmail.com")
+                .telefone("(11) 89345-5687")
+                .dataNascimento(LocalDate.of(1990, 12, 1))
+                .cpf("156.683.240-36")
                 .build();
     }
 
@@ -23,6 +29,10 @@ public class ClienteDados {
         return ClienteDto.builder()
                 .id(1L)
                 .nome("Bruno Silveira")
+                .email("bruno.silveira@gmail.com")
+                .telefone("(11) 89345-5687")
+                .dataNascimento(LocalDate.of(1990, 12, 1))
+                .cpf("156.683.240-36")
                 .build();
     }
 
